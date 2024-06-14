47bff7e39befe286057aae6c9318b340dd7992cb
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2024-06-14 19:12:33 +0700

    add about section:

--- js/script.js
+++ js/script.js
@@ -13,21 +13,32 @@ setInterval(() => {
       }, 1000);
 }, 3000);
 
+
+
 window.onscroll = function () {
       const header = document.querySelector("header");
+      const navMenu = document.querySelector("#nav-menu");
+      
       const fixedNav = header.offsetTop;
+      var width = window.innerWidth;
 
       if (window.pageYOffset > fixedNav) {
             header.classList.add("navbar-fixed");
+            if (width < 1024) {
+                  navMenu.classList.add("navbar-fixed");
+            }
       } else {
             header.classList.remove("navbar-fixed");
+            navMenu.classList.remove("navbar-fixed");
       }
 };
+
+
 
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function(){
+hamburger.addEventListener("click", function () {
       hamburger.classList.toggle("hamburger-active");
       navMenu.classList.toggle("translate-x-full");
 });
